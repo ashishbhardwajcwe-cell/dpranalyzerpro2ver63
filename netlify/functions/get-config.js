e2920cf9ace6b5314158163da04cd2fd2856ee44
ashishbhardwajcwe-cell <ashishbhardwaj.cwe@gmail.com>
2026-05-17 22:25:37 +0530

    Add Netlify function to get configuration

---
+++ netlify/functions/get-config.js
@@ -0,0 +1,10 @@
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    headers: {"Access-Control-Allow-Origin": "*"},
+    body: JSON.stringify({
+      status: "ok",
+      key: process.env.ANTHROPIC_API_KEY
+    })
+  };
+};
